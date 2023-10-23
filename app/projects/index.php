454cc0f6881eae782e35ac29464017cd9c4a9d5c
inspizzz <wiktor.wiejak@gmail.com>
2023-10-23 21:54:07 +0100

    added content to the projects page

--- app/projects/index.php
+++ app/projects/index.php
@@ -4,4 +4,48 @@
             <a> Projects </a>
         </div>
     </div>
+
+    <div class="page-padding projects">
+
+        <!-- 8-bit computer -->
+        <a class="project" href="/projects/8bit">
+            <div>
+                <img class="project-image" src="/resources/8BitWorking.jpg" />
+            </div>
+            
+            <div class="description"> 
+                <h1> 8-bit Computer </h1>
+                <p> I started the 8 bit computer project after seeing a video online showcasing a functional breadboard computer. It caught my attention and made me thing about my first year of university learning about similar concepts, yet these were showcased right in front of my eyes. I was hooked instantly. </p>
+                <p> After doing some more research I stumbled upon some videos made by Ben Eater and looked at his content. His hands on style of explaining concepts caught my attention and I took away much more information from his videos than in any other case. </p>
+                <p> Also being on committee for the computer science society at Exeter, I devised a plan to teach myself as much as possible about 8 bit computers as I can in my first term of academics. Then after christmas and first term exams are over, introduce the content I discovered to our members through intuitive lab sessions every week. </p>
+            </div>
+        </a>
+
+        <!-- mysoc startup -->
+        <a class="project" href="/projects/mysoc">
+            <div>
+                <img class="project-image" src="/resources/MySoc.JPG" />
+            </div>
+            <div class="description"> 
+                <h1> MySoc </h1>
+                <p> Since beginning university teo friends of mine have found incredible difficulty with organising society events, stash, elections and messages. We decided to do something about this and started brainstorming solutions as well as joining a University led programme called the pre-incubator to help with out business knowledge. </p>
+                <p> From there we allocated time each week for scrum meetings to discuss progress that had been made, perform code review and also discuss future tasks at hand that needed to be completed to meet the goals of our MVP. </p>
+                <p> Neering the end of the pre-incubator course, we applied to pitch our startup idea to a panel of judges, both from the pre-incubator and incubator programs. After preparing diligently and practicing our pitch, we were successful and received funding for executing more market research and developing our MVP. </p>
+            </div>
+        </a>
+        
+
+        <!--  -->
+        <a class="project" href="/projects/particles">
+            <div>
+                <!-- <img src="/" /> -->
+            </div>
+            <div class="description"> 
+                <h1> Particle Simulator </h1>
+                <p> In my first year of university I attended extra lectures with some friends from third year natural sciences. Nuclear and high energy particly physics had some very out of reach content about various atomic nuclei models ( liquid drop, shell model) and the pros and cons of each. </p>
+                <p> I enjoyed listening to Professor Euan Hendry speak about the matter greatly. Since then I have thought about physics slightyly more and decided to use html, more specifically the canvas element, in order to demonstrate the movement of particles with mass against each other using forces such as gravity for interactions. </p>
+                <p> The project was an interesting exploration of the concept and I benefited greatly from it. My understanding of html grew and I had great enjoyment from tinkering around with values for force, acceleration and velocity.
+            </div>
+        </a>
+    </div>
 </div>
