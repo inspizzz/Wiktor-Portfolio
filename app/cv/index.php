f173aef3f9557226e43303db3a261a0d89a630b1
inspizzz <wiktor.wiejak@gmail.com>
2023-10-30 00:03:14 +0000

    added cv

---
+++ app/cv/index.php
@@ -0,0 +1,5 @@
+<div id="main">
+    <!-- <object data="/resources/cv.pdf" type="application/pdf" width="300" height="200"> -->
+        <embed src="resources/cv.pdf" type="application/pdf">
+    <!-- </object> -->
+</div>
