39adf3657f795543a96224e78f63c7358145c2ea
inspizzz <wiktor.wiejak@gmail.com>
2023-10-30 00:20:03 +0000

    made pdf work

--- app/cv/index.php
+++ app/cv/index.php
@@ -1,5 +1,16 @@
 <div id="main">
-    <object data="/resources/cv.pdf" type="application/pdf" width="300" height="200">
+    <!-- <object class="container" data="/resources/cv.pdf" type="application/pdf" width="80%" height="80%">
         <embed src="resources/cv.pdf" type="application/pdf">
-    </object>
+    </object> -->
+    <!-- <embed src="resources/cv.pdf" width="100%" height="1000px" type="application/pdf"/> -->
+
+    <!-- <ifrmae class="pdf" src="/resources/cv.pdf" type="application/pdf"></iframe> -->
+    <iframe
+      src="/resources/cv.pdf"
+      width="100%"
+      height="100%"
+      loading="lazy"
+      title="PDF-file"
+      
+  ></iframe>
 </div>
