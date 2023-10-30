03ee4ba229077219a00fb5ec199088553c068fdc
inspizzz <wiktor.wiejak@gmail.com>
2023-10-30 00:07:16 +0000

    made pdf show

--- app/cv/index.php
+++ app/cv/index.php
@@ -1,5 +1,5 @@
 <div id="main">
-    <!-- <object data="/resources/cv.pdf" type="application/pdf" width="300" height="200"> -->
+    <object data="/resources/cv.pdf" type="application/pdf" width="300" height="200">
         <embed src="resources/cv.pdf" type="application/pdf">
-    <!-- </object> -->
+    </object>
 </div>
