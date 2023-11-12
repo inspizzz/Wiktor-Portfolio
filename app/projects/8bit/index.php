92ab0cf271381ef39b5090de940aed1cc71f362d
inspizzz <wiktor.wiejak@gmail.com>
2023-11-12 16:54:50 +0000

    added content for the weekly 8 bit computer sessions blog

---
+++ app/projects/8bit/index.php
@@ -0,0 +1,90 @@
+<!-- blog post type of format -->
+<div id="main">
+    <div class="heading">
+        <h1> 8-Bit Computer! </h1>
+    </div>
+
+    <section class="post">
+        <div class="post-heading">
+            <h2> Our First Meeting! </h2>
+            <p> 11th October 2023 </p>
+        </div>
+
+        <div class="post-body">
+            <div class="post-text">
+                <p> To kick off our project I organised a team meeting where we went through the details of the project and exploring some of the parts we are going to be using. I got everyone to pick a different chip and explore what it does as well as its applicability to the 8-bit computer. Afterwards we looked at assembling an A-stable clock mechanism using the 555 timer ic chip and successfully managed to get two prototypes of this working. The device we built had a built in potentiometer that allowed us to vary the speed of the pulses to be quicker or slower and to see this we placed an LED with a current limiting resistor at the output.</p>
+                <p> I felt that this session was extremely positive as we became more comfortable with out setting in the laboratory but also got to know each other better and apply creativity to build a piece of technology that would fundamentally be used within the computer.
+            </div>
+
+            <!-- slideshow -->
+            <div class="post-images">
+                <img src="/resources/projects/8bit/blog1/collaborating.jpg" />
+            </div>
+        </div>
+    </section>
+
+    <section class="post">
+        <div class="post-heading">
+            <h2> Session 2 - Start Clock </h2>
+            <p> 18th October 2023 </p>
+        </div>
+
+        <div class="post-body">
+            <div class="post-text">
+                <p>After my first successful meeting last week I was happy to invite everyone for the next session! My plans for today were to familiarise us with how exactly the clock works and what components help complete desired functionality and in what way. We looked over my notes and understood the ins and outs of the 555 timer, a key component in the clocks inner workings, and the use of logic gates (74LS04, 74LS08 and 74LS32).</p>
+                <p>The clock architecture I decided to go for was one which allows the user to switch between two modes. The first mode being automatic and the second is a button which the user may toggle through each clock pulse, click by click. The benefits of this is that when debugging the computer, this will come in incredibly useful discovering faulty components and connections.  </p>
+                <p>We have also had a change of scenery too! I attended a talk by Chris Sum at Tech Exeter and met someone who works at the university! She works in a technology centred creative space and has access to a whole heap of cool micro controllers and sensors as well as creative diy tools. I have been very fotunate to have meet her and moving forward we have come to an agreement that our team may use the space for the foreseable future! This room is incredible and provides us with an amazing space to learn and get hands on with any technology related project that anyone here at the university may be working on </p>
+                <p>In conclusion, today our team managed to start building two clock modules. I felt really positive about today and sure that everyone grasped a much better understanding of how the clock works in a fundamental level beyond just the structure of the chip. </p>
+            </div>
+
+            <!-- slideshow -->
+            <div class="post-images">
+                <img src="/resources/projects/8bit/blog2/collaborating.jpg" />
+            </div>
+        </div>
+    </section>
+
+    <section class="post">
+        <div class="post-heading">
+            <h2> Personal - Thinking about the registers </h2>
+            <p> 1th November 2023 </p>
+        </div>
+
+        <div class="post-body">
+            <div class="post-text">
+                <p> During this week most of the people attending the workshops were away so I decided to do some extra work learning about the next steps in the project. </p>
+                <p> In order for the processor to be able to store temporary 8 bit data a register is required. The purpose of it is to temporarily store any sort of sequence of bits that can only be 8 bits long. This may be 8 bits of data relating to an address, instruction, or even data!</p>
+                <p> The register is made up of 8 D-Type flip flops. Each flip flop is used to store a bit of data which fundamentally is constructed using a latching mechanism. Such mechanism may be achieved with the use of nand gates and a clock signal. </p>
+                <p> In order to understand the inner working of the register I decided to build a 1 bit register as can be seen in the image. It uses logic to decide when to store the input data and also has an sr-latch which is used to store the value while there is power. </p>
+            </div>
+
+            <!-- slideshow -->
+            <div class="post-images">
+                <img src="/resources/projects/8bit/blog3/1bit-memory.jpg" />
+            </div>
+        </div>
+    </section>
+
+    <section class="post">
+        <div class="post-heading">
+            <h2> Session 3 - Finish Clock </h2>
+            <p> 8th November 2023 </p>
+        </div>
+
+        <div class="post-body">
+            <div class="post-text">
+                <p> We have had a bit of a break as most of the people attending my workshops went away for reading week ( a time when lectures are put on pause, similar to half term ). Since my last workshop my plan for this one was to hopefully be able to complete the clock and begin to learn about the next steps in the project. </p>
+                <p> Today we finished creating two fully working clock circuits! This is extremely exciting as it shows all the hard work we put into this project so far. There were some updates I had to make to the circuit diagrams due to some missing connections however my team was able to efficiently capture what was going wrong and solve this by implementing a solution extremely efficiently.</p>
+                <p> Now that we have two clocks, we will be moving onto learning about the bus and how registers may interact with it. </p>
+            </div>
+
+            <!-- slideshow -->
+            <div class="post-images">
+                <video width="320" height="240" controls>
+                    <source src="/resources/projects/8bit/blog4/clock.MP4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    </section>
+</div>
